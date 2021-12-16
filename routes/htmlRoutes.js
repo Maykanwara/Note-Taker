66ff21999cf525b633c2ded141a128770fb5d61b
Kanwara Moore <Maykanwara@gmail.com>
2021-12-16 00:19:31 -0800

    updated Note Taker

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,34 @@
+const router = require("express"). Router()
+const path = require("path")
+router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname,  "../public/notes.html"))
+
+})
+
+
+
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/api/notes', function(req, res) {
+//     res.sendFile(path.join(__dirname + "public/notes.html"))
+
+
+// })
